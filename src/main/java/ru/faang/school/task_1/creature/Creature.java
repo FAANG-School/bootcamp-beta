a192ef1dedaa4bc771f208b15f6370cb3062ab3e
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2023-03-22 21:40:26 +0200

    Small styles refactoring for Creature class. Implemented Hero class.

--- src/main/java/ru/faang/school/task_1/creature/Creature.java
+++ src/main/java/ru/faang/school/task_1/creature/Creature.java
@@ -6,68 +6,69 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public abstract class Creature {
-	private String name;
-	private int level;
-	private int attack;
-	private int defense;
-	private int speed;
+    private String name;
+    private int level;
+    private int attack;
+    private int defense;
+    private int speed;
 
-	private int health;
-	private int quantity;
+    private int health;
+    private int quantity;
 
-	public abstract int getDamage();
+    public abstract int getDamage();
 
-	Creature(String name, int level, int quantity, int attack, int defense, int speed, int health) {
-		this.name = name;
-		this.level = level;
-		this.quantity = quantity;
-		this.attack = attack;
-		this.defense = defense;
-		this.speed = speed;
-		this.health = health;
-	}
+    Creature(String name, int level, int quantity, int attack, int defense, int speed, int health) {
+        this.name = name;
+        this.level = level;
+        this.quantity = quantity;
+        this.attack = attack;
+        this.defense = defense;
+        this.speed = speed;
+        this.health = health;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getLevel() {
-		return level;
-	}
+    public int getLevel() {
+        return level;
+    }
 
-	public int getAttack() {
-		return attack;
-	}
+    public int getAttack() {
+        return attack;
+    }
 
-	public int getDefense() {
-		return defense;
-	}
+    public int getDefense() {
+        return defense;
+    }
 
-	public int getSpeed() {
-		return speed;
-	}
+    public int getSpeed() {
+        return speed;
+    }
 
-	public int getHealth() {
-		return health;
-	}
-	public int getQuantity() {
-		return quantity;
-	}
+    public int getHealth() {
+        return health;
+    }
 
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
+    public int getQuantity() {
+        return quantity;
+    }
 
-	@Override
-	public String toString() {
-		return "%s [lvl %s] [q %s]".format(name, level, quantity);
-	}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-	protected int getRandomDamage(int from, int to) {
-		List<Integer> range = IntStream.rangeClosed(from, to)
-				.boxed().collect(Collectors.toList());
-		Random random = new Random();
-		int damageIndex = random.nextInt(range.size());
-		return range.get(damageIndex);
-	}
+    @Override
+    public String toString() {
+        return String.format("%s [lvl %s] [q %s]", name, level, quantity);
+    }
+
+    protected int getRandomDamage(int from, int to) {
+        List<Integer> range = IntStream.rangeClosed(from, to)
+                .boxed().collect(Collectors.toList());
+        Random random = new Random();
+        int damageIndex = random.nextInt(range.size());
+        return range.get(damageIndex);
+    }
 }
--- src/main/java/ru/faang/school/task_1/hero/Hero.java
+++ src/main/java/ru/faang/school/task_1/hero/Hero.java
@@ -1,3 +1,52 @@
-public class Hero {
+package ru.faang.school.task_1.hero;
 
+import ru.faang.school.task_1.creature.Creature;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hero {
+    private String name;
+    private String fraction;
+    private int experience;
+    private int level;
+
+    private Map<Creature, Integer> army;
+
+    public Hero(String name, String fraction, int experience, int level) {
+        this.name = name;
+        this.fraction = fraction;
+        this.experience = experience;
+        this.level = level;
+        this.army = new HashMap<Creature, Integer>();
+    }
+
+    public void addCreature(Creature creature, int quantity) {
+        if (army.containsKey(creature)) {
+            army.put(creature, army.get(creature) + quantity);
+        } else {
+            army.put(creature, quantity);
+        }
+    }
+
+    public void removeCreature(Creature creature, int quantity) {
+        if (army.containsKey(creature)) {
+            army.put(creature, army.get(creature) + quantity);
+            int currentQuantity = army.get(creature);
+            if ((currentQuantity - quantity) <= 0) {
+                army.remove(creature);
+            } else {
+                army.put(creature, army.get(creature) - quantity);
+            }
+        }
+    }
+
+    public Map<Creature, Integer> getArmy() {
+        return army;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[$s] (%s) [lvl %s, exp %s]", name, fraction, level, experience);
+    }
 }
