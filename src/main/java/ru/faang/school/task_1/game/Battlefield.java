d17acbbf263d7b557c01e95560cc5c9aefc50b07
George <gsaranov@gmail.com>
2023-03-23 20:54:54 +0300

    [Task1] - refactoring & changing the startup in the main method to junit tests

--- src/main/java/ru/faang/school/task_1/game/Battlefield.java
+++ src/main/java/ru/faang/school/task_1/game/Battlefield.java
@@ -5,21 +5,17 @@ import ru.faang.school.task_1.game.heroes.Hero;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Battlefield {
-    public static void main(String[] args) {
-        Hero firstHero = new Hero("Hero1", "Red Fraction");
-        firstHero.addCreature(new Angel(), 1);
-        firstHero.addCreature(new Griffin(), 4);
-        firstHero.addCreature(new Pikeman(), 3);
-        firstHero.addCreature(new Swordman(), 5);
+    private Hero firstHero;
 
+    private Hero secondHero;
 
-        Hero secondHero = new Hero("Hero2", "Blue Fraction");
-        secondHero.addCreature(new Angel(), 1);
-        secondHero.addCreature(new Griffin(), 4);
-        secondHero.addCreature(new Pikeman(), 3);
-        secondHero.addCreature(new Swordman(), 5);
+    public Battlefield(Hero firstHero, Hero secondHero) {
+        this.firstHero = firstHero;
+        this.secondHero = secondHero;
+    }
 
-        int lot = ThreadLocalRandom.current().nextInt(1, 2 + 1);
+    public Hero battle() {
+        int lot = draw();
         Hero winner;
         if(lot == 1) {
             System.out.println(firstHero.getName() + " goes first.");
@@ -28,31 +24,10 @@ public class Battlefield {
             System.out.println(secondHero.getName() + " goes first.");
             winner = battle(secondHero, firstHero);
         }
-        System.out.println("Winner is " + winner);
-
-        Hero thirdHero = new Hero("Hero3", "Red Fraction");
-        thirdHero.addCreature(new Angel(), 3);
-
-
-        Hero fourthHero = new Hero("Hero4", "Blue Fraction");
-        fourthHero.addCreature(new Griffin(), 110);
-        fourthHero.addCreature(new Pikeman(), 150);
-        fourthHero.addCreature(new Swordman(), 100);
-
-        lot = ThreadLocalRandom.current().nextInt(1, 2 + 1);
-        if(lot == 1) {
-            System.out.println(thirdHero.getName() + " goes first.");
-            winner = battle(thirdHero, fourthHero);
-        } else {
-            System.out.println(fourthHero.getName() + " goes first.");
-            winner = battle(fourthHero, thirdHero);
-        }
-        System.out.println("Winner is " + winner);
+        return winner;
     }
 
-    public static Hero battle(Hero firstHero, Hero secondHero) {
-        firstHero.getArmy().sort(((o1, o2) -> o2.getSpeed().compareTo(o1.getSpeed())));
-        secondHero.getArmy().sort(((o1, o2) -> o2.getSpeed().compareTo(o1.getSpeed())));
+    private Hero battle(Hero firstHero, Hero secondHero) {
         int strokeNumber = 1;
         while (!firstHero.getArmy().isEmpty() || !secondHero.getArmy().isEmpty()) {
             if(secondHero.getArmy().isEmpty() || firstHero.getArmy().isEmpty()) {
@@ -68,7 +43,15 @@ public class Battlefield {
         return firstHero.getArmy().isEmpty() ? secondHero : firstHero;
     }
 
-    private static void makeAMove(Hero hero, Hero enemyHero) {
+    private int draw() {
+        return ThreadLocalRandom.current().nextInt(1, 2 + 1);
+    }
+    private void sortingArmiesBySpeedDesc(Hero firstHero, Hero secondHero) {
+        firstHero.getArmy().sort(((o1, o2) -> o2.getSpeed().compareTo(o1.getSpeed())));
+        secondHero.getArmy().sort(((o1, o2) -> o2.getSpeed().compareTo(o1.getSpeed())));
+    }
+
+    private void makeAMove(Hero hero, Hero enemyHero) {
         for (Creature unit : hero.getArmy()) {
             if(enemyHero.getArmy().isEmpty()) {
                 return;
