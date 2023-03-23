ded8ee012c81664c728d501a7467d6339fe12b51
Lesn0y <kilriljob@gmail.com>
2023-03-23 10:31:04 +0300

    Add entity Heroes

---
+++ src/main/java/ru/faang/school/task_1/heroes/Hero.java
@@ -0,0 +1,56 @@
+package ru.faang.school.task_1.heroes;
+
+import ru.faang.school.task_1.creatures.Creature;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Hero {
+
+    private final String name;
+    private final Fraction fraction;
+    private int experience;
+    private int level;
+    private List<Creature> army;
+
+    public Hero(String name, Fraction fraction) {
+        this.name = name;
+        this.fraction = fraction;
+        this.experience = 0;
+        this.level = 1;
+    }
+
+    public boolean addCreature(Creature creature) {
+        if (army == null) {
+            army = new LinkedList<>();
+        }
+        return army.add(creature);
+    }
+
+    public boolean removeCreature(Creature creature) {
+        if (army == null) {
+            return false;
+        }
+        return army.remove(creature);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Fraction getFraction() {
+        return fraction;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public List<Creature> getArmy() {
+        return army;
+    }
+}
