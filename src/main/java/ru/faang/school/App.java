ad5225800e78eab9303a5574a87676ebc2edd6bb
Elvin <elvin2002mammedli@gmail.com>
2023-03-23 17:01:36 +0400

    first repository

--- src/main/java/ru/faang/school/App.java
+++ src/main/java/ru/faang/school/App.java
@@ -5,6 +5,5 @@ public class App {
 
 
 
-
     }
 }
--- src/main/java/ru/faang/school/task_1/Creature.java
+++ src/main/java/ru/faang/school/task_1/Creature.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public abstract class Creature {
 
     public static ArrayList<Creature> creatures = new ArrayList<>();
-
     Creature() {
         creatures.add(this);
     }
