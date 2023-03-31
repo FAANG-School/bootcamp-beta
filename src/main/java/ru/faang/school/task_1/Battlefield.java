ead7c01794a5374b37eba5583e5999960782abc7
sub1k <olav123454@yandex.ru>
2023-03-31 18:07:11 +0500

    Done!

--- src/main/java/ru/faang/school/task_1/Battlefield.java
+++ src/main/java/ru/faang/school/task_1/Battlefield.java
@@ -1,5 +1,7 @@
 package ru.faang.school.task_1;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Battlefield {
@@ -12,28 +14,39 @@ public class Battlefield {
     Hero hero2;
 
     String battle(){
-        int scoreFirst = 0;
+        int hero1Power = 0;
+        for(Map.Entry<Creature, Integer> entry : hero1.army.entrySet()){
+           hero1Power += entry.getValue();
+        }
+        int hero2Power = 0;
+        for(Map.Entry<Creature, Integer> entry : hero2.army.entrySet()){
+            hero2Power += entry.getValue();
+        }
 
-        scoreFirst += hero1.army.get(new Swordman()).compareTo(hero2.army.get(new Swordman()));
-        scoreFirst += hero1.army.get(new Angel()).compareTo(hero2.army.get(new Angel()));
-        scoreFirst += hero1.army.get(new Griffin()).compareTo(hero2.army.get(new Griffin()));
-        scoreFirst += hero1.army.get(new Pikeman()).compareTo(hero2.army.get(new Pikeman()));
-        if (scoreFirst > 0){
-            return hero1.name;
-        } else if (scoreFirst == 0) {
-            Random rand = new Random();
-            int res = rand.nextInt(2);
-            switch (res){
-                case 0 -> {
-                    return hero1.name;
-                }
-                case 1 -> {
-                    return hero2.name;
-                }
+        int res = Integer.compare(hero1Power,hero2Power);
+        switch (res){
+            case 1 -> {
+                return hero1.name;
             }
-        } else if (scoreFirst < 0){
-            return hero2.name;
+            case 0 -> {
+                return "Draw!";
+            }
+            case -1 -> {
+                return hero2.name;
+            }
         }
         return null;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Creature, Integer> hero1Army = new HashMap<>();
+        hero1Army.put(new Griffin("Grif"), 100);
+        Hero hero1 = new Hero("Oleg", hero1Army);
+        HashMap<Creature, Integer> hero2Army = new HashMap<>();
+        hero2Army.put(new Swordman("Sword"), 200);
+        Hero hero2 = new Hero("Vlad", hero2Army);
+
+        Battlefield battlefield = new Battlefield(hero1,hero2);
+        System.out.println(battlefield.battle());
     }
 }
