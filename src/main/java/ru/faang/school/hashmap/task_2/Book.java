c097ce9819ef27d878cadaecce099bdb74ebc446
Vanya <vanyailnitsk@yandex.ru>
2023-04-04 22:45:33 +0300

    Book class added

---
+++ src/main/java/ru/faang/school/hashmap/task_2/Book.java
@@ -0,0 +1,39 @@
+package ru.faang.school.hashmap.task_2;
+
+import java.time.Year;
+
+public class Book {
+    private String title;
+    private String author;
+    private Year year;
+
+    public Book(String title, String author, Year year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Year getYear() {
+        return year;
+    }
+
+    public void setYear(Year year) {
+        this.year = year;
+    }
+}
