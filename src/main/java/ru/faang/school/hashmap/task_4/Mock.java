b5cec986849abac11be3c5be2f03eb9b4490c663
MatveyLshkn <lashkinmatveyfw@gmail.com>
2023-04-04 20:18:56 +0300

    update

--- src/main/java/ru/faang/school/hashmap/task_4/Mock.java
+++
@@ -1,6 +0,0 @@
-public class Mock {
-    public static WeatherData findWeather() {
-        System.out.println("Finding info about weather in Tokyo");
-        return new WeatherData("Tokyo",22.22,44.44);
-    }
-}
