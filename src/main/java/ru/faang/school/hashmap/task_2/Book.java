c99d5805f86d08e7edacbdac05baceac60a9944a
Vanya <vanyailnitsk@yandex.ru>
2023-04-05 00:10:22 +0300

    Hashmap task2 (test required)

--- src/main/java/ru/faang/school/hashmap/task_2/Book.java
+++ src/main/java/ru/faang/school/hashmap/task_2/Book.java
@@ -1,13 +1,14 @@
 package ru.faang.school.hashmap.task_2;
 
 import java.time.Year;
+import java.util.Objects;
 
 public class Book {
     private String title;
     private String author;
-    private Year year;
+    private String year;
 
-    public Book(String title, String author, Year year) {
+    public Book(String title, String author, String year) {
         this.title = title;
         this.author = author;
         this.year = year;
@@ -29,11 +30,32 @@ public class Book {
         this.author = author;
     }
 
-    public Year getYear() {
+    public String getYear() {
         return year;
     }
 
-    public void setYear(Year year) {
+    public void setYear(String year) {
         this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(year, book.year);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "{Название: '" + title + '\'' +
+                ", автор: " + author  +
+                ", год издания: " + year +
+                '}';
     }
 }
