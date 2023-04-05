d5977cac11826f9d97f49e80166c39565e9dc4e4
Andrei <surkov.andrew@gmail.com>
2023-04-05 16:45:14 +0200

    task_1

--- src/main/java/ru/faang/school/hashmap/task_1/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_1/Main.java
@@ -4,6 +4,38 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    Map<String, House> map = new HashMap<>();
+    static Map<String, House> houseMap = new HashMap<>();
 
+    public static void addHouse(String name, String sigil) {
+        House house = new House(name.toLowerCase(),sigil.toLowerCase());
+        houseMap.put(name.toLowerCase(),house);
+    }
+
+    public static void deleteHouse(String name) {
+        houseMap.remove(name.toLowerCase());
+    }
+
+    public static void findHouse(String name) {
+        if(houseMap.containsKey(name.toLowerCase())) {
+            System.out.println(houseMap.get(name.toLowerCase()).getSigil());
+        } else {
+            System.out.println(name + " not found");
+        }
+    }
+
+    public static void printAll() {
+        for(var entry : houseMap.entrySet()) {
+            System.out.println(entry.getKey() + ":" + entry.getValue().getSigil());
+        }
+    }
+
+    public static void main(String args[]) {
+        addHouse("Stark", "Wolf");
+        addHouse("Lannister", "Lion");
+        addHouse("Barateon", "Deer");
+        deleteHouse("Lannister");
+        findHouse("Talli");
+        printAll();
+
+    }
 }
