83783d1080a1b99c5cae207b0f3861c60d21be78
Denis Komarov <107692921+DeniSpoilT@users.noreply.github.com>
2023-04-05 21:51:33 +0300

    upload

--- src/main/java/ru/faang/school/hashmap/task_5/WebPage.java
+++ src/main/java/ru/faang/school/hashmap/task_5/WebPage.java
@@ -1,2 +1,48 @@
-package ru.faang.school.hashmap.task_5;public class WebPage {
+package ru.faang.school.hashmap.task_5;
+
+import java.net.URL;
+import java.util.Objects;
+
+public class WebPage {
+    private String url;
+
+    private String title;
+
+    private String content;
+
+    public WebPage(String url, String title, String content) {
+        this.url = url;
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WebPage webPage = (WebPage) o;
+        return Objects.equals(url, webPage.url) && Objects.equals(title, webPage.title) && Objects.equals(content, webPage.content);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url, title, content);
+    }
+
+    @Override
+    public String toString() {
+        return "WebPage{" + url + '}';
+    }
 }
