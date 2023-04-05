15a560f1549b1ec24708214a610f7eaedd080f1d
Andrei <surkov.andrew@gmail.com>
2023-04-05 19:34:29 +0200

    task_1

--- src/main/java/ru/faang/school/hashmap/task_1/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_1/Main.java
@@ -29,7 +29,7 @@ public class Main {
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         addHouse("Stark", "Wolf");
         addHouse("Lannister", "Lion");
         addHouse("Barateon", "Deer");
