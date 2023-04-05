03d5395a35ba62fd0d2ee864eb356fd12be072a4
Verefrint <DimaVe2004@gmail.com>
2023-04-05 12:47:56 +0300

    Verefrint, 1 task hashmap

---
+++ src/main/java/ru/faang/school/hashmap/task_1/House.java
@@ -0,0 +1,30 @@
+package ru.faang.school.hashmap.task_1;
+
+public class House
+{
+    private String name;
+
+
+    private String sigil;
+    public House (String name, String sigil){
+        this.sigil=sigil;
+        this.name=name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSigil() {
+        return sigil;
+    }
+
+    public void setSigil(String sigil) {
+        this.sigil = sigil;
+    }
+
+}
+
