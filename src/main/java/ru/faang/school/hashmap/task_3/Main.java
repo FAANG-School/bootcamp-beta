9860508c876262476095d45aa8403857dc11b8fa
kislyakoff <kislyakoff@tut.by>
2023-04-05 19:56:14 +0300

    init commit for task_3

---
+++ src/main/java/ru/faang/school/hashmap/task_3/Main.java
@@ -0,0 +1,10 @@
+package ru.faang.school.hashmap.task_3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/ru/faang/school/hashmap/task_3/Student.java
@@ -0,0 +1,31 @@
+package ru.faang.school.hashmap.task_3;
+
+public class Student {
+	
+	private final String name;
+	private final String faculty;
+	private final Integer year;
+	
+	public Student(String name, String faculty, Integer year) {
+		this.name = name;
+		this.faculty = faculty;
+		this.year = year;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getFaculty() {
+		return faculty;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+	
+	
+	
+	
+
+}
