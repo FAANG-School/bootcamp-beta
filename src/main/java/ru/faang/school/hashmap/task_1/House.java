acfaa863a114066b9e005aa76a80e497ab3a5c3e
yamkin29 <127436193+yamkin29@users.noreply.github.com>
2023-04-05 21:54:45 +0500

    Add files via upload

---
+++ src/main/java/ru/faang/school/hashmap/task_1/House.java
@@ -0,0 +1,10 @@
+public class House {
+    public String name;
+    public String sigil;
+
+    public House(String name, String sigil){
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+}
