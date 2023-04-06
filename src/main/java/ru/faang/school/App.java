3144b5396856a6526fd25350ce2114ddf5fbd772
ROCKET228 <michkahap@gmail.com>
2023-04-06 20:04:39 +0300

    Start first hashmap task, create hashmap houseMap, creat 3 object of class House and add them to hashmap houseMap

--- src/main/java/ru/faang/school/App.java
+++ src/main/java/ru/faang/school/App.java
@@ -1,7 +1,22 @@
 package ru.faang.school;
 
+import ru.faang.school.hashmap.task_1.House;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) {
+        //HashMap Task 1
+        Map<String, House> houseMap = new HashMap<>();
+        House lannister = new House("Lannister", "Lion");
+        House stark = new House("Stark", "Wolf");
+        House baratheon = new House("Baratheon", "Deer");
+        houseMap.put("Lannister", lannister);
+        houseMap.put("Stark", stark);
+        houseMap.put("Baratheon", baratheon);
+
+
 
     }
 }
---
+++ src/main/java/ru/faang/school/hashmap/task_1/House.java
@@ -0,0 +1,13 @@
+package ru.faang.school.hashmap.task_1;
+
+public class House {
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public House(){}
+}
