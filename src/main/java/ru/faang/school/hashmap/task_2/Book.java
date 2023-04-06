e36b1654dd5889dacb666632c8e42f331df4ffe7
tiptopkid <skeeveb@gmail.com>
2023-04-06 20:28:47 +0500

    task2

---
+++ src/main/java/ru/faang/school/hashmap/task_2/Book.java
@@ -0,0 +1,56 @@
+package ru.faang.school.hashmap.task_2;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    @Override
+    public String toString() {
+        return  "Название- '" + title + '\'' +
+                ", Автор- '" + author + '\'' +
+                ", Год издания- " + year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        if (year != book.year) return false;
+        if (!Objects.equals(title, book.title)) return false;
+        return Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + year;
+        return result;
+    }
+
+
+}
