16badd7d03477631094dee5324b526a9bc60ad2c
tiptopkid <skeeveb@gmail.com>
2023-04-06 17:20:41 +0500

    task 1

--- src/main/java/ru/faang/school/App.java
+++ src/main/java/ru/faang/school/App.java
@@ -1,10 +1,8 @@
 package ru.faang.school;
 
-import ru.faang.school.task_1.model.creature.Pikeman;
 
 public class App {
     public static void main(String[] args) {
-//        Pikeman pikeman = new Pikeman();
-//        System.out.println(pikeman.getAttack());
+
     }
 }
