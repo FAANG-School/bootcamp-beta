0527ad89e387b3f9c125d95a5dd6276c1cd74fc5
mishasc2ml <mkokarev989898@gmail.com>
2023-04-06 15:32:14 +0300

    hashmap task1 + tests implemented

---
+++ src/main/java/ru/faang/school/hashmap/task_1/Main.java
@@ -0,0 +1,48 @@
+package ru.faang.school.hashmap.task_1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+    private Map<String, House> houses = new HashMap<>() {{
+        put("Stark", new House("Stark", "Wolf"));
+        put("Lannister", new House("Lannister", "Lion"));
+        put("Barateon", new House("Barateon", "Deer"));
+        put("Targaryen", new House("Targaryen", "Dragon"));
+    }};
+
+    public Map<String, House> getHouses() {
+        return houses;
+    }
+
+    public void addNewHouse(String houseName, House house) {
+        houses.put(houseName, house);
+    }
+
+    public void deleteHouse(String houseName) {
+        if (houses.containsKey(houseName)) {
+            houses.remove(houseName);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public String getSigil(String houseName) {
+        if (houses.containsKey(houseName)) {
+            return houses.get(houseName).getSigil();
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public List<String> getAllHousesInfo() {
+        List<String> res = new ArrayList<>();
+        for (Map.Entry<String, House> entry : houses.entrySet()) {
+            res.add(entry.getKey() + entry.getValue());
+        }
+        return res;
+    }
+}
