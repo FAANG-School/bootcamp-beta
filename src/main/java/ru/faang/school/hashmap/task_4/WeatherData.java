84a70d1ec0336e5ad967dbb26ce253da0db39d14
mishasc2ml <mkokarev989898@gmail.com>
2023-04-06 19:46:12 +0300

    hashmap task 4 + tests implementation

---
+++ src/main/java/ru/faang/school/hashmap/task_4/WeatherData.java
@@ -0,0 +1,62 @@
+package ru.faang.school.hashmap.task_4;
+
+import java.util.Objects;
+
+public class WeatherData {
+
+    private String city;
+    private int temperature;
+    private String humidity;
+
+    public WeatherData(String city, int temperature, String humidity) {
+        this.city = city;
+        this.temperature = temperature;
+        this.humidity = humidity;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+        this.humidity = humidity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeatherData that = (WeatherData) o;
+        return temperature == that.temperature && Objects.equals(city, that.city) && Objects.equals(humidity, that.humidity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, temperature, humidity);
+    }
+
+    @Override
+    public String toString() {
+        return "WeatherData{" +
+                "city='" + city + '\'' +
+                ", temperature=" + temperature +
+                ", humidity=" + humidity +
+                '}';
+    }
+}
