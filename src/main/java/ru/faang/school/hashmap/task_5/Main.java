bb6e7b4fbef99b324fdfc6e7b52d181c59597bb1
kislyakoff <kislyakoff@tut.by>
2023-04-07 11:15:59 +0300

    init commit task_5

---
+++ src/main/java/ru/faang/school/hashmap/task_5/Main.java
@@ -0,0 +1,15 @@
+package ru.faang.school.hashmap.task_5;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+	
+	private final Map<String, List<WebPage>> map = new HashMap<>();
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/ru/faang/school/hashmap/task_5/WebPage.java
@@ -0,0 +1,18 @@
+package ru.faang.school.hashmap.task_5;
+
+public class WebPage {
+	
+	private final String url;
+	private final String title;
+	private final String content;
+	
+	public WebPage(String url, String title, String content) {
+		this.url = url;
+		this.title = title;
+		this.content = content;
+	}
+	
+	
+	
+
+}
