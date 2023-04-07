eef00c34f412d9d8c639d57f5ed37f8fe4e2ccc7
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-04-07 21:43:59 +0600

    actions with books

---
+++ src/main/java/ru/faang/school/hashmap/task_2/BookNotFoundException.java
@@ -0,0 +1,4 @@
+package ru.faang.school.hashmap.task_2;
+
+public class BookNotFoundException extends RuntimeException{
+}
---
+++ src/main/java/ru/faang/school/hashmap/task_2/Main.java
@@ -0,0 +1,18 @@
+package ru.faang.school.hashmap.task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        BookDAO bookDAO = new BookDAO();
+        bookDAO.add(new Book("Rich Dad Poor Dad", "Robert Kiyosaki", 1997), "#12");
+        bookDAO.add(new Book("Effective Java", "Joshua Bloch", 2001), "#43");
+        bookDAO.add(new Book("They Both Die at the end", "Adam Silvera", 2017), "#341");
+
+        bookDAO.printAll();
+        System.out.println("-----------Find book-------------------");
+        bookDAO.find("They Both Die at the end", "Adam Silvera", 2017);
+        System.out.println("----------Remove book-----------------------");
+        bookDAO.remove("Effective Java", "Joshua Bloch", 2001);
+        System.out.println("-------------Print all books--------------------------");
+        bookDAO.printAll();
+    }
+}
