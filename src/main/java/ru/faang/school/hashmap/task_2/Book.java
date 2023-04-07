d5b9c97b8beec460bd4bdcb1fb1fbec6b164858a
Danil Pudovkin <dvpudovkin@gmail.com>
2023-04-07 21:56:13 +0500

    refactor: del redundant code from Book record

--- src/main/java/ru/faang/school/hashmap/task_2/Book.java
+++ src/main/java/ru/faang/school/hashmap/task_2/Book.java
@@ -1,9 +1,6 @@
 package ru.faang.school.hashmap.task_2;
 
-import java.util.Objects;
-
 public record Book(String title, String author, int year) {
-
     public Book {
         if (title == null || title.isBlank()) {
             throw new IllegalArgumentException("title cannot be null or empty");
@@ -14,22 +11,5 @@ public record Book(String title, String author, int year) {
         if (year <= 0) {
             throw new IllegalArgumentException("year cannot be <= 0");
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(year, book.year);
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                ", year=" + year +
-                '}';
     }
 }
