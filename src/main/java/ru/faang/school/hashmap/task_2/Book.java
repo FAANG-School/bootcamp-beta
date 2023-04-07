7b5f0aa2ae77e4ef18bd8216dd14cfdf7e8713d9
ROCKET228 <michkahap@gmail.com>
2023-04-07 16:10:25 +0300

    Make task 2 in hashmap

---
+++ src/main/java/ru/faang/school/hashmap/task_2/Book.java
@@ -0,0 +1,40 @@
+package ru.faang.school.hashmap.task_2;
+
+import java.util.Objects;
+
+class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book (){}
+    public Book (String title, String author, int year){
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return year == book.year && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, year);
+    }
+
+    public String getTitle(){
+        return this.title = title;
+    }
+    public String getAuthor(){
+        return this.author = author;
+    }
+
+    public int getYear(){
+        return this.year = year;
+    }
+}
