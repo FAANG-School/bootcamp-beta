59f5ab0b10ca36528bc46307095ddd422bf69afe
Danil Pudovkin <dvpudovkin@gmail.com>
2023-04-07 21:56:37 +0500

    refactor: del redundant code from House record

--- src/main/java/ru/faang/school/hashmap/task_1/House.java
+++ src/main/java/ru/faang/school/hashmap/task_1/House.java
@@ -1,9 +1,6 @@
 package ru.faang.school.hashmap.task_1;
 
-import java.util.Objects;
-
 public record House(String name, String sigil) {
-
     public House {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException("name cannot be null or empty");
@@ -11,21 +8,5 @@ public record House(String name, String sigil) {
         if (sigil == null || sigil.isBlank()) {
             throw new IllegalArgumentException("sigil cannot be null or empty");
         }
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        House house = (House) o;
-        return Objects.equals(name, house.name) && Objects.equals(sigil, house.sigil);
-    }
-
-    @Override
-    public String toString() {
-        return "House{" +
-                "name='" + name + '\'' +
-                ", sigil='" + sigil + '\'' +
-                '}';
     }
 }
