2021dfc04c34e5649d301b40bf5d4b0b69237566
Alizhan Zhaumbay <moneymanever@gmail.com>
2023-04-08 19:24:49 +0600

    models, exceptions

---
+++ src/main/java/ru/faang/school/hashmap/task_3/Faculty.java
@@ -0,0 +1,8 @@
+package ru.faang.school.hashmap.task_3;
+
+public enum Faculty {
+    BUSINESS_SCHOOL,
+    ENGINEERING_AND_NATURAL_SCIENCES,
+    EDUCATION_AND_HUMANITIES,
+    LAW_AND_SOCIAL_SCIENCES
+}
