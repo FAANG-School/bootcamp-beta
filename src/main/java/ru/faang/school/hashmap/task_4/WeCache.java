e808f531473076a5aaa97e8f8b6ccb5776fd9de6
ROCKET228 <michkahap@gmail.com>
2023-04-10 16:38:03 +0300

    Make weatherInfo method to output info about weather by city name

--- src/main/java/ru/faang/school/hashmap/task_4/WeCache.java
+++ src/main/java/ru/faang/school/hashmap/task_4/WeCache.java
@@ -1,4 +1,34 @@
 package ru.faang.school.hashmap.task_4;
 
+
+import java.util.*;
+
+
 public class WeCache {
+
+    public static void main(String[] args){
+        Map<String, WeatherData> weatherDataMap = new HashMap<>();
+        WeatherData kyiv = new WeatherData("Kyiv", 20 ,15);
+        weatherDataMap.put(kyiv.getCity(), kyiv );
+        weatherDataMap.put("Borispol", new WeatherData("Borispol", 26 ,28) );
+        weatherDataMap.put("Brovaru", new WeatherData("Brovaru", 18,33) );
+        weatherDataMap.put("Dnipro", new WeatherData("Dnipro", 13 ,25) );
+        System.out.println(weatherInfo(weatherDataMap, "Kyiv"));
+    }
+
+    public static String weatherInfo(Map<String, WeatherData> weatherDataMap, String city){
+        for (Map.Entry<String, WeatherData> entry : weatherDataMap.entrySet()) {
+            if (city.equals(entry.getValue().getCity())) {
+                WeatherData weatherData = entry.getValue();
+                return "Temperature: " + weatherData.getTemperature() + ", Humidity: " + weatherData.getHumidity();
+            }
+        }
+
+        weatherDataMap.put(city, new WeatherData(city , (int) Math.random() * 3, (int) Math.random() * 3));
+
+        return  "Information about weather in " + city + " cached.";
+
+    }
+
+
 }
