3177b309df5548ca66ef2b9c6d3a36de4bb383f2
kislyakoff <kislyakoff@tut.by>
2023-04-11 00:39:51 +0300

    code review correction

--- src/main/java/ru/faang/school/hashmap/task_3/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_3/Main.java
@@ -1,37 +1,39 @@
 package ru.faang.school.hashmap.task_3;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Main {
-	
-	private static final List<Student> students = new ArrayList<>();
-	
+
+	private static final Set<Student> STUDENTS = new HashSet<>();
+	private static Map<Tuple, Set<Student>> STUDENTS_MAP = new HashMap<>();
+
 	static {
-		students.add(new Student("Jack London", "Literature", 2020));
-		students.add(new Student("Bob Marley", "Music", 2021));
-		students.add(new Student("Donald Ervin Knuth", "Computer Science", 2020));
-		students.add(new Student("Joshua J. Bloch", "Computer Science", 2023));
-		students.add(new Student("James Gosling", "Computer Science", 2020));
-		students.add(new Student("Andy Warhol", "Design", 2022));
-		
+		STUDENTS.add(new Student("Jack London", "Literature", 2020));
+		STUDENTS.add(new Student("Bob Marley", "Music", 2021));
+		STUDENTS.add(new Student("Donald Ervin Knuth", "Computer Science", 2020));
+		STUDENTS.add(new Student("Joshua J. Bloch", "Computer Science", 2023));
+		STUDENTS.add(new Student("James Gosling", "Computer Science", 2020));
+		STUDENTS.add(new Student("Andy Warhol", "Design", 2022));
+		STUDENTS_MAP = mapByFacultyAndYear();
 	}
-	
+
 	private static class Tuple {
-		
+
 		private final String faculty;
 		private final Integer year;
-		
-		Tuple(String faculty, Integer year) {
+
+		private Tuple(String faculty, int year) {
 			this.faculty = faculty;
 			this.year = year;
 		}
-		
-		Integer getYear() {
+
+		private int getYear() {
 			return year;
 		}
 
@@ -57,98 +59,109 @@ public class Main {
 			return "faculty: " + faculty + ", year: " + year;
 		}
 	}
-	
-	public static Map<Tuple, List<Student>> mapByFacultyAndYear() {
-		
-		return students.stream()
-					   .collect(Collectors.groupingBy(s -> new Tuple(s.getFaculty(), s.getYear()), 
-							    Collectors.toList()));
+
+	public static Map<Tuple, Set<Student>> mapByFacultyAndYear() {
+
+		return STUDENTS.stream().collect(Collectors
+				.groupingBy(student -> new Tuple(student.getFaculty(), student.getYear()), Collectors.toSet()));
 	}
-	
+
 	public static void add(Student student) {
 		if (student == null)
-			throw new IllegalArgumentException("Null args");
-		
-		if (students.contains(student))
+			throw new IllegalArgumentException("Student can't be null");
+
+		if (STUDENTS.contains(student)) {
 			System.out.println("This student already exists");
-		else
-			students.add(student);
+		} else {
+			Tuple key = new Tuple(student.getFaculty(), student.getYear());
+			STUDENTS.add(student);
+			STUDENTS_MAP.computeIfAbsent(key, set -> new HashSet<>()).add(student);
+		}
 	}
-	
+
 	public static void remove(String name, String faculty, Integer year) {
-		if (name == null || faculty == null || year == null)
+		if (name == null || faculty == null || year == null) {
 			throw new IllegalArgumentException("Null args");
+		}
 		
 		Student student = new Student(name, faculty, year);
 		
-		if (!students.remove(student))
+		if (!STUDENTS.remove(student)) {
 			System.out.println("No such student in list");
+		}
+		else {
+			STUDENTS_MAP.values().forEach(value -> value.remove(student));
+			/* STUDENTS_MAP.values().removeIf(Set::isEmpty); */ // uncomment if it needs to remove key with empty value
+				
+		}
 	}
-	
-	public static List<Student> searchByFacultyAndYear(String faculty, Integer year) {
-		if (faculty == null || year == null)
+
+	public static Set<Student> searchByFacultyAndYear(String faculty, Integer year) {
+		if (faculty == null || year == null) {
 			throw new IllegalArgumentException("Null args");
-		
-		
-		return mapByFacultyAndYear().get(new Tuple(faculty, year));
+		}
+		return STUDENTS_MAP.get(new Tuple(faculty, year));
 	}
-	
+
 	public static void printByFacultyAndYear() {
-		
-		if(students.isEmpty())
+
+		if (STUDENTS.isEmpty())
 			System.out.println("Students list is empty");
 		else
 			mapByFacultyAndYear().entrySet().stream()
-				.sorted(Map.Entry.comparingByKey(Comparator.comparing(Tuple::getYear)))
-				.forEach(e -> {
-					System.out.println(e.getKey() + "\n  Students list:");
-					e.getValue()
-					  .forEach(System.out::println);
-					System.out.println();
-				});
-		}
-	
-	/* just for testing. 
-	 * pending delete */
+					.sorted(Map.Entry.comparingByKey(Comparator.comparing(Tuple::getYear))).forEach(e -> {
+						System.out.println(e.getKey() + "\n  Students list:");
+						e.getValue().forEach(System.out::println);
+						System.out.println();
+					});
+	}
+
+	/*
+	 * just for testing. pending delete
+	 */
 	public static void clearTest() {
-		students.clear();
+		STUDENTS.clear();
 	}
-	
-	/* just for testing. 
-	 * pending delete */
+
+	/*
+	 * just for testing. pending delete
+	 */
 	public static int sizeTest() {
-		return students.size();
+		return STUDENTS.size();
 	}
-	
-	/* just for testing. 
-	 * pending delete */
+
+	/*
+	 * just for testing. pending delete
+	 */
 	public static boolean containsTest(Student student) {
-		return students.contains(student);
+		return STUDENTS.contains(student);
 	}
-	
-	/* just for testing. 
-	 * pending delete */
+
+	/*
+	 * just for testing. pending delete
+	 */
 	public static void removeTest(Student student) {
-		students.remove(student);
+		STUDENTS.remove(student);
 	}
-	
-	/* just for testing. 
-	 * pending delete */
+
+	/*
+	 * just for testing. pending delete
+	 */
 	public static void addTest(Student student) {
-		students.add(student);
+		STUDENTS.add(student);
 	}
 
 	public static void main(String[] args) {
-		
-		
-		/* unsorted print map just for testing. 
-		 * pending delete */
+
+		/*
+		 * unsorted print map just for testing. pending delete
+		 */
 		mapByFacultyAndYear().forEach((k, v) -> {
 			System.out.println(k + "\n  Students list: ");
 			v.forEach(System.out::println);
 			System.out.println();
 		});
-		
+
 	}
 
 }
--- src/main/java/ru/faang/school/hashmap/task_3/Student.java
+++ src/main/java/ru/faang/school/hashmap/task_3/Student.java
@@ -3,12 +3,12 @@ package ru.faang.school.hashmap.task_3;
 import java.util.Objects;
 
 public class Student {
-	
+
 	private final String name;
 	private final String faculty;
-	private final Integer year;
-	
-	public Student(String name, String faculty, Integer year) {
+	private final int year;
+
+	public Student(String name, String faculty, int year) {
 		this.name = name;
 		this.faculty = faculty;
 		this.year = year;
@@ -22,11 +22,9 @@ public class Student {
 		return faculty;
 	}
 
-	public Integer getYear() {
+	public int getYear() {
 		return year;
 	}
-	
-	
 
 	@Override
 	public int hashCode() {
@@ -42,17 +40,11 @@ public class Student {
 		if (getClass() != obj.getClass())
 			return false;
 		Student other = (Student) obj;
-		return Objects.equals(faculty, other.faculty) && Objects.equals(name, other.name)
-				&& Objects.equals(year, other.year);
+		return Objects.equals(faculty, other.faculty) && Objects.equals(name, other.name) && year == other.year;
 	}
 
 	@Override
 	public String toString() {
 		return name;
 	}
-	
-	
-	
-	
-
 }
