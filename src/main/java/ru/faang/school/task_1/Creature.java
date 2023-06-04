2502d7e5095ec792ea438e503315bf3b8796f339
Dmitry Sorokin <dm.sorokin21@gmail.com>
2023-06-04 16:55:42 +0300

    created abstract class Creature

---
+++ src/main/java/ru/faang/school/task_1/Creature.java
@@ -0,0 +1,14 @@
+package ru.faang.school.task_1;
+
+public abstract class Creature {
+    public String name;
+    public int level;
+    public int attack;
+    public int defense;
+    public int speed;
+    public int countCreature;
+    public int getDamage() {
+        return attack;
+    }
+
+}
