fd67733832d4c5b3a688f2b042a54d19f3c2708e
Devilserker <demonserker@gmail.com>
2023-06-09 13:14:31 +0600

    Исправил ветку, теперь здесь только задач номер 2

--- src/main/java/ru/faang/school/task_1/Creature/Pikeman.java
+++
@@ -1,7 +0,0 @@
-package ru.faang.school.task_1.Creature;
-
-public class Pikeman extends Creature{
-    public Pikeman(){
-        super("Pikeman", 1, 10, 5, 5);
-    }
-}
--- src/main/java/ru/faang/school/task_1/Creature/Swordman.java
+++
@@ -1,7 +0,0 @@
-package ru.faang.school.task_1.Creature;
-
-public class Swordman extends Creature{
-    public Swordman(){
-        super("Swordman", 1, 8,6,8);
-    }
-}
