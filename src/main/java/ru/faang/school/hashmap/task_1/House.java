a88213c1b038c2126f6c9bb4ccd5c1eccb91fe4a
svetlana.obrancova <obrancova.s@gmail.com>
2023-06-11 16:58:31 +0200

    HM-t1-1 Add classes House, Main

---
+++ src/main/java/ru/faang/school/hashmap/task_1/House.java
@@ -0,0 +1,27 @@
+package ru.faang.school.hashmap.task_1;
+
+public class House {
+    private String name;
+    private String sigil;
+
+    public House(String name, String sigil) {
+        this.name = name;
+        this.sigil = sigil;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSigil() {
+        return sigil;
+    }
+
+    @Override
+    public String toString() {
+        return " {" +
+                "name='" + name + '\'' +
+                ", sigil='" + sigil + '\'' +
+                '}';
+    }
+}
