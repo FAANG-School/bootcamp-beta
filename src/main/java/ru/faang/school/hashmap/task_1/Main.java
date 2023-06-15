8e6db87a90e2139237d799501a0964d589ee84c9
svetlana.obrancova <obrancova.s@gmail.com>
2023-06-15 17:27:05 +0200

    HM-t1-3 Убрала Set, вместо него создала новый м-д displayAllHouses(); поправила м-д displaySigilByHouseName()

--- src/main/java/ru/faang/school/hashmap/task_1/Main.java
+++ src/main/java/ru/faang/school/hashmap/task_1/Main.java
@@ -2,43 +2,41 @@ package ru.faang.school.hashmap.task_1;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 public class Main {
-    private static final HashMap<String, House> HOUSE_MAP = new HashMap<>();
+    private static final HashMap<String, House> HOUSE = new HashMap<>();
 
     public static void main(String[] args) {
-
         House lannisterHouse = new House("Lannister", "Lion");
         House starkHouse = new House("Stark", "Direwolf");
         House baratheonHouse = new House("Baratheon", "Stag");
         House tullyHouse = new House("Tully", "Trout");
         House tyrellHouse = new House("Tyrell", "Rose");
 
-        HOUSE_MAP.put("Lannister", lannisterHouse);
-        HOUSE_MAP.put("Stark", starkHouse);
-        HOUSE_MAP.put("Baratheon", baratheonHouse);
-        HOUSE_MAP.put("Tully", tullyHouse);
-        HOUSE_MAP.put("Tyrell", tyrellHouse);
+        HOUSE.put("Lannister", lannisterHouse);
+        HOUSE.put("Stark", starkHouse);
+        HOUSE.put("Baratheon", baratheonHouse);
+        HOUSE.put("Tully", tullyHouse);
+        HOUSE.put("Tyrell", tyrellHouse);
 
         House targaryenHouse = new House("Targaryen", "Dragon");
-        HOUSE_MAP.put("Targaryen", targaryenHouse);
+        HOUSE.put("Targaryen", targaryenHouse);
 
-        HOUSE_MAP.remove("Tully");
+        HOUSE.remove("Tully");
 
-        displaySigilByHouseName(HOUSE_MAP, "Parrot");
-        displaySigilByHouseName(HOUSE_MAP, "Baratheon");
+        displayAllHouses();
+        displaySigilByHouseName("Parrot");
+        displaySigilByHouseName("Baratheon");
+    }
 
-        Set<Map.Entry<String, House>> entriesHouses = HOUSE_MAP.entrySet();
-        for (Map.Entry<String, House> entry : entriesHouses) {
+    private static void displayAllHouses() {
+        for (Map.Entry<String, House> entry : HOUSE.entrySet()) {
             System.out.println("House: " + entry.getKey() + " --->" + entry.getValue());
         }
     }
-    private static void displaySigilByHouseName(Map<String, House> HOUSE_MAP, String name) {
-        House house = HOUSE_MAP.get(name);
-
-        if (house != null) {
-            System.out.println("Sigil of " + name + ": " + house.getSigil());
+    private static void displaySigilByHouseName(String name) {
+        if (HOUSE.containsKey(name)) {
+            System.out.println("Sigil of " + name + ": " + HOUSE.get(name).getSigil());
         } else {
             System.out.println("House not found: " + name);
         }
